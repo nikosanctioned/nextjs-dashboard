c0e64d0bdb7f0e1de1c432f5daec3e20bfab9ba8
nikosanctioned <swiftpythondataai@icloud.com>
2024-01-31 16:53:51 +0300

    final commit related to metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+import { invoices } from '@/app/lib/placeholder-data';
+
+export const metadata: Metadata = {
+  title: "Nikolai's invoices",
+};
 
 export default async function Page({
   searchParams,
