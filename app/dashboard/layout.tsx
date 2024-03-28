9462b5f2672af9103a3dfd1a0d8a5f5774b07c7e
nikosanctioned <swiftpythondataai@icloud.com>
2024-03-28 15:04:24 +0300

    sd

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,21 +3,15 @@ import React, { ReactNode } from 'react';
 
 interface LayoutProps {
   children: ReactNode;
-  firstpage: ReactNode;
-  secondpage: ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, firstpage, secondpage }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        {children}
-        {firstpage}
-        {secondpage}
-      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
 };
