b8d25e68f6e3e94eee8909083db2852b9bbdfb68
nikosanctioned <swiftpythondataai@icloud.com>
2024-04-09 19:01:34 +0300

    fixed eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+const withMDX = require('@next/mdx')();
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -9,6 +10,7 @@ const nextConfig = {
     instrumentationHook: true,
     typedRoutes: true,
   },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withMDX(withBundleAnalyzer(nextConfig));
